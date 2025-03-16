67e1c375693407e48c1736e11906bbd0b1e9d444
Afonkin Pavel <pvafonkin@edu.hse.ru>
2025-03-16 23:29:50 +0000

    add lib to pypi

---
+++ hw_2/library/README.md
@@ -0,0 +1,1 @@
+Lib for education purposes
--- hw_2/scripts/main.py
+++ hw_2/scripts/main.py
@@ -1,4 +1,4 @@
-import hw_2.lib.texgen as texgen
+import texgen as texgen
 from string import Template
 
 _TEX_TEMPLATE = Template("""\\documentclass[letterpaper,11pt]{article}                         
@@ -13,7 +13,7 @@ $image
 """)
 
 def main():
-    file = open("artifacts/document.tex", "w")
+    file = open("../artifacts/document.tex", "w")
     file.write(_TEX_TEMPLATE.substitute(
         table=texgen.generate_table([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]),
         image=texgen.generate_image("../data/demotivator.png")
